761fb933dd2951d77fa72631d12d7859f842508b
Emerslopess <emerslopess@gmail.com>
2025-02-19 00:55:52 +0000

    Gauruja

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,54 @@
 //Teste larissa
 
 int main() {
+    // Carta 1 
+    char estado[] = "SP";  // Estado
+    char codigo_da_carta[]= A01; // codigo da carta
+    char nome_da_cidade[]= "Guaruja"; // Cidade
+    int populacao = 287634; // População de Guaruja
+    float area = 142.589; // area Área (em km²)
+    float pib =2.29090186589; // Pib da cidade Guaruja
+    int numero_de_ponto_turistico = 15, // Total de pontos turisticos 
+
+
+    // Carta 01 SP- Guaruja
+
+    printf("Digite o Estado:");
+    scanf("%c", &estado);
+
+    printf("Digite o Codigo da Carta:"); 
+    scanf("%s", codigo_da_cidade);
+
+    printf("Digite Nome da Cidade:");
+    scanf("%s", nome_da_cidade);
+
+
+    printf("Digite a Populoação da Cidade:");
+    scanf("%d", &populacao);
+
+
+    printf("Digite a área da cidade (em km²):");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da Cidade:");
+    scanf("%f", &pib);
+
+    printf("Digite Numero de Pontos Turisticos da cidade:")
+    scanf("%d", numero_de_ponto_turistico);
+
+
+    // Dados Da Carta do Gaurujá
+    printf("\n---Cidade ---\n");
+    printf("Estado: %c\n", estado);
+    printf("Codigo da Carta: %s\n", codigo_da_cidade);
+    printf("Nome da Cidade: %s\n", nome_da_cidade);
+    printf("População: %d\n", populacao);
+    printf("Área (em km²): %.2f\n",area);
+    printf("PIB da Cidade: %.2f", pib);
+    printf("Numero de Pontos Turistico: %d\n", numero_de_ponto_turistico);
+
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
